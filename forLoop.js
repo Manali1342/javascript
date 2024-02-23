bd42d90798b34a5a12e84e87307d0ce1719766ca
ManaliPatil <patilmanali121@gmail.com>
2024-02-23 13:11:17 +0530

    fifth commit

---
+++ forLoop.js
@@ -0,0 +1,42 @@
+//FOR LOOP
+// for(let i=0;i<34;i++){
+//     console.log(i);
+// }
+
+
+//PROGRAM TO ADD FIRST N NATURAL NUMBER
+//LET IS BLOCK SCOPED
+// let sum = 0;
+// for(let i=0;i<30;i++)
+// {
+//     sum+=i;
+// }
+// console.log(sum);
+// console.log(i);
+
+//VAR IS GLOBALLY SCOPED
+let sum = 0;
+for(var i=0;i<30;i++)
+{
+    sum+=i;
+}
+console.log(i);
+
+
+//FOR IN LOOP
+// let obj={
+//     Sumit: 90,
+//     Jivika: 67,
+//     Raj: 78,
+//     Shiv: 88,
+//     Radhika: 89
+// }
+// for(let a in obj){
+//     // console.log(obj[a]);
+//     console.log("The marks of "+a+" are "+obj[a]);
+// }
+
+//FOR OFF LOOP
+// for(let b of "Sumit"){
+//     console.log(b)
+// }
