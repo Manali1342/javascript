f3942840cf8eaffed1b3baa85d0c8d33611b7650
ManaliPatil <patilmanali121@gmail.com>
2024-02-23 13:08:22 +0530

    second commit

---
+++ arrMethods.js
@@ -0,0 +1,30 @@
+//Convert array to string
+let num= [2,6,9,4,1];
+// let b = num.toString();
+// console.log(b);
+// console.log(typeOf(b));
+
+//join method
+// let c = num.join("and");
+// console.log(c);
+// console.log(typeOf(c));
+
+//POP method
+// let p = num.pop();//It returns the popped element....and  it update the original array
+// console.log(num,p);
+
+//Push metthod
+// let d = num.push(78);//Push returns the new array length..
+// console.log(num,d);
+
+//Shift Method
+// let r = num.shift();
+// console.log(num,r);//Remove first element and return it...its update the original array
+
+//unShift Method
+// let r = num.unshift(90);//Add element to the starting of the array...and update the original array
+// console.log(num,r);
+
+
+
+
