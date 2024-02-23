fd9729e0d347f1b5961334df256308ec3c86b6f4
ManaliPatil <patilmanali121@gmail.com>
2024-02-23 13:13:29 +0530

    eighth commit

---
+++ strMethods.js
@@ -0,0 +1,46 @@
+// let name = "Sumit\"";
+// console.log(name.length);
+// let name = "Sumit";
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// Slice
+// console.log(name.slice(2,4));
+// console.log(name.slice(2));
+
+// Replace
+// let name = "Harry Bhai";
+// console.log(name.replace("Bhai","Bhau"));
+
+// Concat
+let name = "Aniket";
+let friend = "Naman";
+console.log(name.concat("is a friend of ",friend ," ok"));
+console.log(name.concat("is friend of "+friend));
+
+//Trim
+// let friend2 = "    Meena        ";
+// console.log(friend2);
+// console.log(friend2.trim());
+
+//Strings are immutable.In order to  access the character at anindex we use following syntax....
+// let frd = "Shivika"+" Harry";
+// console.log(frd);
+// let fr = "Shivika";
+// console.log(fr[0]);
+// console.log(fr[1]);
+// console.log(fr[2]);
+// console.log(fr[3]);
+// console.log(fr[4]);
+// console.log(fr[5]);
+// console.log(fr[6]);
+// console.log(fr);
+
+//QUIZ-- Use for loop to print a string....
+// let fname = "Manali";
+// for(let i=0;i<fname.length;i++)
+// {
+//     console.log(fname[i]);
+// }
+
+
+
