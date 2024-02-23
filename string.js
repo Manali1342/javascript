03e2ac07ac813a804e4e1f222d8ba32aa865c083
ManaliPatil <patilmanali121@gmail.com>
2024-02-23 13:14:43 +0530

    tenth commit

---
+++ string.js
@@ -0,0 +1,28 @@
+// let name = "Manali";
+// console.log(name);
+// console.log(name[0]);
+// console.log(name[1]);
+// console.log(name[2]);
+// console.log(name[3]);
+// console.log(name[4]);
+// console.log(name[5]);
+// console.log(name.length);
+
+// let friend = 'Priyanka';
+// console.log(friend);
+
+//Template Literals
+// let boy1 = "Promod";
+// let boy2 = "Nikhil";
+// let sentence = `${boy1} is a friend of ${boy2}`; //String Interpolation
+// console.log(sentence);
+
+
+//Escape sequence character \n \t 
+// let fruit = 'Bana\'na';
+// let fruits = "Bana\nna";
+// console.log(fruit);
+// console.log(fruits);
+
+
+
