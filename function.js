39f1819ca697d21e24f52116dc611187c3943b6d
ManaliPatil <patilmanali121@gmail.com>
2024-02-23 13:12:04 +0530

    sixth commit

---
+++ function.js
@@ -0,0 +1,11 @@
+let a = 2;
+let b = 3;
+let c = 7;
+function onePlusAvg(x,y){
+    return 1+(x+y)/2
+    console.log("done")
+}
+console.log("Average of a and b is ",onePlusAvg(a,b));
+console.log("Average of b and c is ",onePlusAvg(b,c));
+console.log("Average of a and c is ",onePlusAvg(a,c));
+
