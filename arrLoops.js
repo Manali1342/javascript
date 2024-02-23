4a7bbe603b52baba363b2ddd60591014262e9128
ManaliPatil <patilmanali121@gmail.com>
2024-02-23 13:05:44 +0530

    first commit

---
+++ arrLoops.js
@@ -0,0 +1,28 @@
+// let arr = [2,7,5,9,1];
+// FOR LOOP
+// for(let i =0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+//FOR EACH LOOP
+// arr.forEach((element)=>{
+//     console.log(element*element);
+// })
+
+//Array from --->Used to create an array from other object..
+// let name = "Manali";// String or HTML Collection
+// let arr = Array.from(name);
+// console.log(arr);
+
+//  FOR OFF LOOP
+let num = [4,8,9,5,2];
+// for(let item of num){
+//     console.log(item);
+// }
+
+//FOR IN LOOP
+for(let i in num){
+    console.log(num[i]);
+}
+
+
